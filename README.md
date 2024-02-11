669afbecaff932a41193922cf924a15d57577f53
aKrivonogova <krivonogova.aline@yandex.ru>
2024-02-12 01:25:25 +0300

    added tree services

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# test-task-tree-services
+# Тестовое задание - дерево услуг 
+
+### Описание
+Отрисовка дерева 
+
+# Инструкция по установке:
+
+```sh
+git clone https://github.com/aKrivonogova/mesto
+```
+```sh
+cd mesto
+```
+```sh
+npm i webpack --save-dev
+```
+```sh
+npm run build
+```
+```sh
+npm run dev
+
+
---
+++ src/index.js
@@ -0,0 +1,54 @@
+// массив услуг 
+
+import servicesCollection from "./servicesCollection";
+import './style.css'; // добавьте импорт главного файла стилей
+        // создание элемента 
+        function createElement(elementSelector){
+            return document.createElement(elementSelector)
+        }
+
+        // сортировка коллекции по полю 
+        function sortCollectionByField(dataCollection, field){
+            return dataCollection.sort((collectionItemA, collectionItemB) => collectionItemA[field] - collectionItemB[field])
+        }
+
+        // фильтрация коллекции по условию 
+        function filterCollectionByCondition(dataCollection, fieldA, conditionElement){
+            return dataCollection.filter(collectionItem => collectionItem[fieldA] === conditionElement); 
+        }
+
+        // Добавление нового элемента в DOM 
+        function appendChildToContainer(containerElement, childElement){
+            containerElement.appendChild(childElement); 
+        }
+
+        function buildServicesList(servicesCollection, parentId, listElementContainer){
+           // создать первый список
+            const elementsList = createElement('ul'); 
+            // Отфильтровать корневые узлы 
+            let filteredServicesCollection = filterCollectionByCondition(servicesCollection, 'head',  parentId);
+            // Отсортировать по возрастанию поля 
+            let sorterServicesCollection = sortCollectionByField(filteredServicesCollection, 'sorthead');
+         
+            sorterServicesCollection.forEach((serviceItem) => {
+                // Создание элемента списка 
+                const listItem = createElement('li');
+                // Вывод информации
+                listItem.textContent = `${serviceItem.name} - (${serviceItem.price})`;
+                // Добавление элемента в список 
+                appendChildToContainer(elementsList,listItem)
+                // Если это узел 
+                if(serviceItem.node === 1){
+                    // Повторить функцию 
+                    buildServicesList(servicesCollection,serviceItem.id,listItem)
+                }
+            });
+            // Добавить список в контейнер 
+            appendChildToContainer(listElementContainer,elementsList)
+            return elementsList;
+        }
+
+ const TREE_SERVICES_CONTAINER = 'tree-services-container'; 
+ const listElementContainer = document.getElementById(TREE_SERVICES_CONTAINER);
+
+buildServicesList(servicesCollection.services, null, listElementContainer);
